bd61de5053b9803ad7cdefc7bd6e272e8998aee0
shibuya takumasa <z-shibuya@taitan.jp>
2018-04-23 13:26:32 +0900

    :fire: Replace parts/pager-archive.php code to only pagination function.

--- app/src/php/parts/pager-archive.php
+++ app/src/php/parts/pager-archive.php
@@ -1,11 +1,3 @@
 <?php
-  $big = 999999999999999999;
-  $pagination_setting = array(
-    'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
-    'format' => '?page=%#%',
-    'current' => max(1, get_query_var('paged')),
-    'total' => $wp_query->max_num_pages,
-    'type' => 'list'
-  );
-  echo paginate_links($pagination_setting);
+  number_pagenations();
 ?>
